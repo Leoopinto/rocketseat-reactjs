d66e2bd2d05bbb30d56fdeefbba09c72fcb97645
Leonardo Pinto <leopirespinto@gmail.com>
2019-06-28 09:10:42 -0300

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from "react";
+// eslint-disable-next-line
+import Routes from './routes'
+
+import Header from './components/Header';
+import Main from './pages/main';
+
+import "./styles.css"
+
+
+const App = () => (
+  <div className="App">
+      <Header/>
+      <Routes />
+
+    </div>
+)
+
+
+export default App;
